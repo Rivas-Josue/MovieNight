866673209f4ae95a5eebb5efab76520a082ad477
Josue Rivas <jfr28@byui.edu>
2018-03-24 22:33:52 -0600

    Creating a better toolbar to I can create a nice menu bar

--- app/src/main/java/com/example/jfriv/movienight/MainActivity.java
+++ app/src/main/java/com/example/jfriv/movienight/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.jfriv.movienight;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,9 +10,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //setting the toolbar
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
         //Josue Rivas - my comment to commit to the repository
         //Gama Tellez - my comment :)
         //cor16004 - my comment
+    }
+
+    public void setSupportActionBar(Toolbar myToolbar) {
     }
 }
